9e1f35f15aae78fca09b9b4f6b82c829116176ee
Truong Pham Xuan <xuantruonglk02@gmail.com>
2025-09-18 23:51:56 +0700

    update

--- mockupInfos.js
+++ mockupInfos.js
@@ -2,6 +2,10 @@ const data = {
     'Youth Sweatshirt Gildan 18000B (Made in US)': '18000b',
     'Unisex Jersey Tank Bella Canvas 3480 (Made in US)': '3480',
     'Classic Unisex Crew-neck Sweatshirt Comfort Colors 1566 (Made in US)': '1566',
+    'Unisex V-neck T-shirt Gildan 64V00 (Made in EU)': '64v00',
+    'Unisex Jersey Short Sleeve Tee Bella Canvas 3001 (Made in US)': '3001',
+    'Heavyweight Youth T-Shirt Comfort Colors 9018 (Made in US)': '9018',
+    'Classic Unisex T-Shirt Comfort Colors 1717 (Made in US)': '3001',
 }
 
 const getAvailableMockups = () => {

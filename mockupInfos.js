9a85d5abdd954a737edcf303fd90d0e203ac62ab
Truong Pham Xuan <truongpx@foobla.com>
2025-09-23 10:41:45 +0700

    update

--- mockupInfos.js
+++ mockupInfos.js
@@ -1,11 +1,9 @@
 const data = {
-    'Youth Sweatshirt Gildan 18000B (Made in US)': '18000b',
-    'Classic Unisex Crew-neck Sweatshirt Comfort Colors 1566 (Made in US)': '1566',
-    // 'Unisex Jersey Tank Bella Canvas 3480 (Made in US)': '3480',
-    // 'Unisex V-neck T-shirt Gildan 64V00 (Made in EU)': '64v00',
-    // 'Unisex Jersey Short Sleeve Tee Bella Canvas 3001 (Made in US)': '3001',
-    // 'Heavyweight Youth T-Shirt Comfort Colors 9018 (Made in US)': '9018',
-    // 'Classic Unisex T-Shirt Comfort Colors 1717 (Made in US)': '3001',
+    'Youth Sweatshirt Gildan 18000B (Made in US)': '18000bus',
+    'Classic Unisex Crew-neck Sweatshirt Comfort Colors 1566 (Made in US)': '1566us',
+    'Women\'s T-shirt Gildan 5000L (Made In US)': '5000lus',
+    'Unisex Jersey Tank Bella Canvas 3480 (Made in US)': '3480us',
+    'Heavyweight Youth T-Shirt Comfort Colors 9018 (Made in US)': '9018us',
 }
 
 const getAvailableMockups = () => {
